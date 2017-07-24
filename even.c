fbe85f3fd3f5bb03d94d62d250e546c23fcfb78f
nivethaaarthi98 <30368440+nivethaaarthi98@users.noreply.github.com>
2017-07-24 18:56:29 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(0
+{
+int num,m=20,n=40;
+clrscr();
+printf("print even numbers in a given range m to n");
+for(num=m;num<=n;num++)
+{
+if(num%2==1)
+printf("%d",num);
+}
+getch();
+}
